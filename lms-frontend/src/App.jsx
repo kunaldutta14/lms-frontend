d76f41fcc06ca49b1db3a604bf964baf05e42cc0
Kunal Dutta <dutta@DESKTOP-CJ5L034.(none)>
2024-11-15 18:47:40 +0530

    Added react hot toast

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      
+      <Routes>
+        {
+          /* <Route path="/" element={<Home/>}> </Route> */
+        }
+
+      </Routes>
     </>
   )
 }
--- lms-frontend/src/main.jsx
+++ lms-frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { Toaster } from 'react-hot-toast';
+import App from './App.jsx'; // Component imports
+import './index.css'; // CSS imports
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <App/>
+  <Toaster/>
+  </BrowserRouter>
+);
