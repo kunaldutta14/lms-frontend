0d5fdd40f396e784d6bd851a4da6975f6c82e051
Kunal Dutta <dutta@DESKTOP-CJ5L034.(none)>
2024-11-16 09:15:53 +0530

    Added footer components

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -1,17 +1,12 @@
-import { Routes } from 'react-router-dom'
-import './App.css'
-
+import './App.css';
+import { Routes } from 'react-router-dom';
+import Footer from './components/Footer';
 function App() {
   
 
   return (
     <>
-      <Routes>
-        {
-          /* <Route path="/" element={<Home/>}> </Route> */
-        }
-
-      </Routes>
+      <Footer/>
     </>
   )
 }
